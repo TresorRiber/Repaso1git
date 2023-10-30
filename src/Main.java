c60c62d7e1bb73991150d40787bf02f14f30151b
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:57:12 +0100

    segunda versión

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello papi!");
+        System.out.println("papi!");
     }
+    int i=0;
+    int j=1;
 
+    char lost;
 }
